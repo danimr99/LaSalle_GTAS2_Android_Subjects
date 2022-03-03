7d068f176fc6a88d99d485bc694bfd5544820d9c
Daniel Muelle <danimr1999@gmail.com>
2022-03-03 20:32:19 +0100

    MainActivity completed and intent to SubjectDetails working

---
+++ app/src/main/java/com/subjects/controller/SubjectDetails.java
@@ -0,0 +1,16 @@
+package com.subjects.controller;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.subjects.R;
+
+public class SubjectDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_subject_details);
+    }
+}
--- app/src/main/java/com/subjects/model/Subject.java
+++ app/src/main/java/com/subjects/model/Subject.java
@@ -3,17 +3,32 @@ package com.subjects.model;
 import android.graphics.drawable.Drawable;
 
 public class Subject {
-    private final Drawable image;
-    private final String name;
-    private final String description;
+    //private final Drawable image;
+    private final int name;
+    private final int description;
     private final int credits;
-    private final String professorName;
+    private final int professorName;
 
-    public Subject(Drawable image, String name, String description, int credits, String professorName) {
-        this.image = image;
+    public Subject(Drawable image, int name, int description, int credits, int professorName) {
+        //this.image = image;
         this.name = name;
         this.description = description;
         this.credits = credits;
         this.professorName = professorName;
+    }
+
+    public Subject(int name, int description, int credits, int professorName) {
+        this.name = name;
+        this.description = description;
+        this.credits = credits;
+        this.professorName = professorName;
+    }
+
+    public int getName() {
+        return name;
+    }
+
+    public int getDescription() {
+        return description;
     }
 }
